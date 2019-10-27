f92ccd807707e5abfd67b6bfa90f9f366d36d710
Sam Gendler <sgendler@ignw.io>
2019-10-27 11:18:07 -0700

    Fix broken custom_rules resource in firewall module

--- modules/fabric-net-firewall/main.tf
+++ modules/fabric-net-firewall/main.tf
@@ -9,8 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -109,7 +108,7 @@ resource "google_compute_firewall" "allow-tag-https" {
 #                                dynamic rules                                 #
 ################################################################################
 
-resource "google_compute_firewall" "custom" {
+resource "google_compute_firewall" "custom_allow" {
   # provider                = "google-beta"
   for_each                = var.custom_rules
   name                    = each.key
@@ -135,6 +134,25 @@ resource "google_compute_firewall" "custom" {
       ports    = rule.value.ports
     }
   }
+}
+
+resource "google_compute_firewall" "custom_deny" {
+  # provider                = "google-beta"
+  for_each                = var.custom_rules
+  name                    = each.key
+  description             = each.value.description
+  direction               = each.value.direction
+  network                 = var.network
+  project                 = var.project_id
+  source_ranges           = each.value.direction == "INGRESS" ? each.value.ranges : null
+  destination_ranges      = each.value.direction == "EGRESS" ? each.value.ranges : null
+  source_tags             = each.value.use_service_accounts || each.value.direction == "EGRESS" ? null : each.value.sources
+  source_service_accounts = each.value.use_service_accounts && each.value.direction == "INGRESS" ? each.value.sources : null
+  target_tags             = each.value.use_service_accounts ? null : each.value.targets
+  target_service_accounts = each.value.use_service_accounts ? each.value.targets : null
+  disabled                = lookup(each.value.extra_attributes, "disabled", false)
+  priority                = lookup(each.value.extra_attributes, "priority", 1000)
+  # enable_logging          = lookup(each.value.extra_attributes, "enable_logging", false)
 
   dynamic "deny" {
     for_each = [for rule in each.value.rules : rule if each.value.action == "deny"]
